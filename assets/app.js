4f1ccd21f1a5eebde4e4bc0212394ed43c447f81
Kevin <kboliver82@yahoo.com>
2015-07-30 20:12:48 -0500

    create an object with channel info and finally run that through a function that adds a new row to the screen instead of trying to do it piecemeal.

--- assets/app.js
+++ assets/app.js
@@ -1,45 +1,56 @@
-var channels = [
-  {name: 'FreeCodeCamp', alias: 'freecodecamp'}
-];
+var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","comster404","brunofin","thomasballinger","noobs2ninjas","beohoff", "gamesdonequick"];
 var twitchStreamsUrl = 'https://api.twitch.tv/kraken/streams/';
 var twitchChannelsUrl = 'https://api.twitch.tv/kraken/channels/';
 
 
-function updateChannelStatus(channel, channelInfo) {
-    var testBox = $('#test');
-    var isOnline = channelInfo.streams ? true : false;
-    var channelUrl = 'https://twitch.tv/' + channel.alias;
-  
-    var link = $('<a>').attr('href', channelUrl).text(channel.name + ' is ' + (isOnline ? 'online' : 'offline'));
-    testBox.append(link);
-}
 
-function updateChannelIcon(channel, channelInfo) {
-    var testBox = $('#test');
-    var channelLogo = channelInfo.logo;
-  
-    var image = $('<img>').attr('src', channelLogo).attr('style', 'height: 50px');
-    testBox.prepend(image);
+function addChannelElement(channelInfo) {
+    var container = $('#mainContainer');
+    var rowDiv = $('<div class="row">');
+    var logoDiv = $('<div class="col-md-1">');
+    var nameDiv = $('<div class="col-md-2">');
+    var statusDiv = $('<div class="col-md-2">');
+
+    var image = $('<img>').attr('src', channelInfo.image).attr('style', 'height: 50px');
+    var link = $('<a>').attr('href', channelInfo.url).text(channelInfo.isOnline ? 'online - ' + channelInfo.stream.game : 'offline');
+
+    logoDiv.append(image);
+    nameDiv.append(channelInfo.displayName);
+    statusDiv.append(link);
+    
+    rowDiv.append($('<div class="col-md-4">'));
+    rowDiv.append(logoDiv);
+    rowDiv.append(nameDiv);
+    rowDiv.append(statusDiv);
+    container.append(rowDiv);
 }
 
 function addChannel(channel) {
+    var channelInfo = {};
+
     var xhr = $.ajax({
     type: 'GET',
-    url: twitchStreamsUrl + channel.alias,
+    url: twitchStreamsUrl + channel,
     dataType: 'jsonp',
     contentType: 'application/json'
   }).then(function(data) {
-    updateChannelStatus(channel, data);
+    console.log('stream', data);
+    channelInfo.isOnline = data.stream ? true : false;
+    channelInfo.stream = data.stream;
+    channelInfo.url = 'https://twitch.tv/' + channel;
     
     return $.ajax({
       type: 'GET',
-      url: twitchChannelsUrl + channel.alias,
+      url: twitchChannelsUrl + channel,
       dataType: 'jsonp',
       contentType: 'application/json'
     });
   }).then(function(data) {
-    console.log('data', data);
-    updateChannelIcon(channel, data);
+    console.log('channel', data);
+    channelInfo.image = data.logo;
+    channelInfo.displayName = data.display_name;
+
+    addChannelElement(channelInfo);
   });
 }
 
